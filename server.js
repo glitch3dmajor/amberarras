17ae1a5f1de49716420f1d89e8240fc28dfd2402
Glitch (thread-ubiquitous-nannyberry) <none>
2022-02-26 20:41:46 +0000

    📉🎢 Checkpoint ./server.js:42613295/27 ./lib/definitions.js:42613081/821

--- server.js
+++ server.js
@@ -4600,7 +4600,7 @@ var maintainloop = (() => {
         if (ran.chance(1 -  0.5 * census.crasher / room.maxFood / room.nestFoodAmount)) {
             let spot, i = 30;
             do { spot = room.randomType('nest'); i--; if (!i) return 0; } while (dirtyCheck(spot, 100));
-            let type = (ran.dice(80)) ? ran.chioose([Class.sentryGun, Class.sentrySwarm, Class.sentryTrap]) : ([Class.crasher, Class.divider]);
+            let type = (ran.dice(80)) ? ran.chioose([Class.sentryGun, Class.sentrySwarm, Class.sentryTrap]) : Class.crasher;
             let o = new Entity(spot);
                 o.define(type);
                 o.team = -100;
