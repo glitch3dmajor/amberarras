0e2dfd20f16f81d396f69baad98b403b3ffdcfa4
Glitch (tacocat-server) <none>
2022-03-02 13:41:53 +0000

    🌻🕌 Checkpoint ./server.js:42613295/760 ./config.json:42613295/76

--- server.js
+++ server.js
@@ -2299,8 +2299,7 @@ class Entity {
                 (this.team !== -1 && room.isIn('bas1', loc)) ||
                 (this.team !== -2 && room.isIn('bas2', loc)) ||
                 (this.team !== -3 && room.isIn('bas3', loc)) ||
-                (this.team !== -4 && room.isIn('bas4', loc))
-               (this.team !== -4 && room.isIn('bas4', loc))
+                (this.team !== -4 && room.isIn('bas4', loc))               
             ) { this.kill(); }
         }
     }
