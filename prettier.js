3f278996c9e55e3455dc26a116f231825ed362c8
Glitch (tacocat-server) <none>
2022-03-16 10:54:59 +0000

    🌧🍳 Checkpoint ./lib/definitions.js:42613295/9502 ./prettier.js:42613295/34805

--- prettier.js
+++ prettier.js
@@ -1,1 +1,332 @@
-  
+exports.bossbranch3.UPGRADES_TIER_2 = [
+  //ok I'm done
+  exports.CK1,
+  exports.CK2,
+  exports.CK3,
+  exports.CK4,
+  exports.CK5,
+  exports.CK6,
+  exports.CK7,
+  exports.CK8,
+  exports.bossbranch4,
+];
+
+exports.bossbranch4.UPGRADES_TIER_3 = [exports.weaponist];
+
+exports.dev.UPGRADES_TIER_2 = [
+  //gira bosses branch from page 5 or 6 cant remem
+  exports.basic,
+  exports.notverybasic,
+  exports.notverybasic360,
+  exports.magnet,
+  exports.arena,
+  exports.pages,
+  exports.bad,
+  exports.antitank,
+  exports.minibosses,
+  exports.dominators,
+  exports.noleader,
+  exports.onleader,
+];
+
+exports.GIRA.UPGRADES_TIER_2 = [
+  //gira ive created this testbed for you, the token is in the ENV file under the "SECRETDEV" one
+  exports.basic,
+  exports.notverybasic,
+  exports.notverybasic360,
+  exports.nova,
+  exports.supernova,
+  exports.arena,
+  exports.pages,
+  exports.bad,
+  exports.antitank,
+  exports.minibosses,
+  exports.dominators,
+  exports.noleader,
+  exports.onleader,
+];
+
+exports.S1X7FlF13Y.UPGRADES_TIER_2 = [
+  exports.basic,
+  exports.notverybasic,
+  exports.notverybasic360,
+  exports.arena,
+  exports.pages,
+  exports.bad,
+  exports.antitank,
+  exports.minibosses,
+  exports.dominators,
+  exports.noleader,
+  exports.onleader,
+];
+exports.minibosses.UPGRADES_TIER_3 = [
+  exports.kinetic,
+  exports.eternal,
+  exports.infinitusbranch,
+  exports.kulziribranch,
+  exports.plaguestarterbranch,
+  exports.AS1,
+  exports.microscope,
+  exports.knockout,
+  exports.BX1,
+  exports.predatorY,
+  exports.aries,
+  exports.taurus,
+  exports.bossbranch3,
+];
+exports.minibossesT123.UPGRADES_TIER_3 = [
+  exports.kinetic,
+  exports.eternal,
+  exports.infinitusbranch,
+  exports.kulziribranch,
+  exports.plaguestarterbranch,
+  exports.AS1,
+  exports.microscope,
+  exports.knockout,
+  exports.BX1,
+  exports.predatorY,
+  exports.aries,
+  exports.taurus,
+  exports.bossbranch3,
+];
+
+exports.testbed.UPGRADES_TIER_2 = [
+  exports.basic,
+  exports.arena,
+  exports.pagesT1,
+  exports.dev2,
+  exports.bad,
+  exports.minibossesT123,
+  exports.dominators,
+];
+exports.testbed2.UPGRADES_TIER_2 = [
+  exports.basic,
+  exports.arena,
+  exports.pagesT2,
+  exports.minibossesT132,
+  exports.dominators,
+];
+exports.testbed3.UPGRADES_TIER_2 = [
+  exports.basic,
+  exports.pagesT3,
+  exports.minibossesT123,
+  exports.dominators,
+];
+exports.pagesT1.UPGRADES_TIER_2 = [
+  exports.page2,
+  exports.page3,
+  exports.page4,
+  exports.page5,
+];
+exports.pagesT2.UPGRADES_TIER_2 = [exports.page2, exports.page3];
+exports.pagesT3.UPGRADES_TIER_2 = [exports.page2];
+/*exports.bossthingT123_UPGRADES_TIER_2 = [
+  exports.palisade, 
+  exports.elite_sprayer,
+  exports.elite_destroyer,
+  exports.elite_gunner,
+  exports.elite_drone,
+  exports.elite_sprayernew,
+  exports.division,
+  exports.eligos,
+  exports.elite_battleship,
+  exports.portal,
+  exports.ek1,
+  exports.bossbranch2T123,
+]; */
+exports.dominators.UPGRADES_TIER_3 = [
+  exports.dominatordestroy,
+  exports.dominatorgunner,
+  exports.dominatortrapper,
+  exports.dominatornailgun,
+  exports.dominatordrone,
+  exports.dominatorswarm,
+  exports.dominatoremerger,
+  exports.dominatorauto,
+  exports.dominatorblackhole,
+  exports.dominatorbodyguard,
+];
+
+exports.kulziribranch.UPGRADES_TIER_1 = [
+  exports.ship12,
+  exports.battleshipmk100,
+  exports.kulzirigun,
+  exports.kulziricannon,
+  exports.kulziricannonR,
+  exports.kulziricannonL,
+  exports.kulzirifactory,
+  exports.kulzirirailgun,
+];
+
+exports.pages.UPGRADES_TIER_2 = [
+  exports.page2,
+  exports.page3,
+  exports.page4,
+  exports.page5,
+  exports.page6,
+  exports.page7,
+  exports.page8,
+  exports.page9,
+  exports.page10,
+];
+
+exports.infinitusbranch.UPGRADES_TIER_2 = [
+  exports.Infinitus,
+  exports.infinitusredistributorlayer,
+  exports.infinitusrangerlayer,
+  exports.infinitusflatterlayer,
+  exports.infinitussidewind,
+  exports.infinitusfactorylayer,
+  exports.infinitustripletlayer,
+  exports.infinituscarrierlayer,
+  exports.infinitusmissilelayer,
+];
+
+exports.bad.UPGRADES_TIER_2 = [exports.bushyata, exports.abezthibou];
+
+exports.arena.UPGRADES_TIER_2 = [
+  exports.arenacloser,
+  exports.arenabooster,
+  exports.arenaanni,
+  exports.arenaocto,
+  exports.arenaram,
+  exports.acfactory,
+];
+
+exports.bossbranchnew.UPGRADES_TIER_3 = [
+  exports.elite_sprayer,
+  exports.elite_destroyer,
+  exports.elite_gunner,
+  exports.elite_drone,
+  exports.elite_sprayernew,
+  exports.elite_battleship,
+  exports.eligos,
+  exports.elite_legion,
+  exports.portal,
+  exports.ek1,
+  exports.eternal_crasher,
+  exports.bossbranch2,
+];
+
+exports.bossbranch2.UPGRADES_TIER_2 = [
+  exports.eternal,
+  exports.infinitusbranch,
+  exports.kulziribranch,
+  exports.plaguestarterbranch,
+  exports.AS1,
+  exports.microscope,
+  exports.knockout,
+  exports.BX1,
+  exports.predatorY,
+  exports.aries,
+  exports.taurus,
+  exports.bossbranch3,
+];
+
+exports.BX1.UPGRADES_TIER_3 = [exports.BX2];
+
+exports.BX2.UPGRADES_TIER_3 = [exports.BX3];
+
+exports.bossbranchnewT123.UPGRADES_TIER_2 = [
+  exports.palisade,
+  exports.elite_sprayer,
+  exports.elite_destroyer,
+  exports.elite_gunner,
+  exports.elite_drone,
+  exports.elite_sprayernew,
+  exports.division,
+  exports.eligos,
+  exports.elite_battleship,
+  exports.portal,
+  exports.ek1,
+  exports.bossbranch2T123,
+];
+
+exports.bossbranch2T123.UPGRADES_TIER_2 = [
+  exports.eternal,
+  exports.AS1,
+  exports.microscope,
+  exports.knockout,
+  exports.BX1,
+  exports.predatorY,
+  exports.aries,
+  exports.taurus,
+];
+
+exports.AS1.UPGRADES_TIER_3 = [exports.AS2];
+
+exports.AS2.UPGRADES_TIER_3 = [exports.AS3];
+
+exports.AS3.UPGRADES_TIER_3 = [exports.AS4];
+
+exports.AS4.UPGRADES_TIER_3 = [exports.AS5];
+
+exports.plaguestarterbranch.UPGRADES_TIER_3 = [
+  exports.disease,
+  exports.infection,
+  exports.plaguestarter,
+];
+
+exports.spikePound.UPGRADES_TIER_2 = [
+  exports.spikeBiggerPound,
+  exports.spikedirector,
+];
+
+exports.page2.UPGRADES_TIER_2 = [
+  exports.healer,
+  exports.railgun,
+  exports.moddedinfinitustriplet,
+  exports.moddedinfinituscarrier,
+  exports.moddedinfinitusmissilelauncher,
+  exports.carnivore,
+  exports.blowtorch,
+  exports.spitling,
+  exports.spewer,
+  exports.machinegungunner,
+  exports.programmer,
+];
+
+exports.page3.UPGRADES_TIER_3 = [
+  exports.pulse,
+  exports.sad,
+  exports.sus,
+  exports.fogger,
+  exports.hurricane,
+  exports.Tetracontagon,
+  exports.underlord,
+  exports.collapser,
+  exports.strider,
+  exports.e,
+];
+
+exports.page4.UPGRADES_TIER_3 = [
+  exports.hover,
+  exports.janitor,
+  exports.weatherballoon,
+  exports.pyromaniac,
+  exports.repellant,
+  exports.driller,
+  exports.pentapenta,
+  exports.gas,
+  exports.headlesshorseman,
+];
+
+exports.page5.UPGRADES_TIER_3 = [
+  exports.gammaray,
+  exports.snipingshotgun,
+  exports.grenadeshotgun,
+  exports.telescope,
+  exports.overbliterator,
+];
+
+exports.page6.UPGRADES_TIER_3 = [exports.pages, exports.page7];
+exports.page10.UPGRADES_TIER_3 = [
+  exports.bulkzark,
+  exports.citadel,
+  exports.foodSpawner,
+  exports.constructassassin,
+  exports.nova,
+  exports.supernova,
+  exports.amalgam,
+];
+//43478
